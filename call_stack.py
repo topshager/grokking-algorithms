2fbca8e5fcc87362d01b21d902ae612b039a8c59
topshager <rem28777@gmail.com>
2025-03-20 21:23:55 +0200

    Create call_stack.py

---
+++ call_stack.py
@@ -0,0 +1,13 @@
+name = "lemon"
+def greet(name):
+    print ("hello," + name + "|")
+    greet2(name)
+    print ("getting ready to say bye....")
+    bye()
+
+def greet2(name):
+    print("How are you," + name)
+
+
+def bye():
+    print("ok bye")
