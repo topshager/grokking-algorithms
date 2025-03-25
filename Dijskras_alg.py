0914dd829ee8db0bb42f7895ad1cf992809a6dc0
topshager <rem28777@gmail.com>
2025-03-25 23:34:11 +0200

    Create Dijskras_alg.py

---
+++ Dijskras_alg.py
@@ -0,0 +1,21 @@
+gragh  = {}
+
+
+#store all neighbours of node
+
+
+gragh["you"] = ["alice","bob","clair"]
+
+gragh["start"] = {}
+gragh["start"]["a"] = 6
+gragh["start"] ["b"] = 2
+
+#getting the values from the hash table
+print(gragh["start"].keys())
+
+#weight of edges
+
+print(gragh["start"]["a"])
+
+#adding all nodes
+
