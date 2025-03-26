f6707240b1730db07e3e6705708eacdf5eef9ec9
topshager <rem28777@gmail.com>
2025-03-26 19:18:51 +0200

    create fruits ,py

---
+++ fruits.py
@@ -0,0 +1,11 @@
+fruits = set(["avacodo","tomato","banana"])
+
+vegetables = set(["beets","carrots","tomato"])
+fruits  | vegetables #this sets a union
+set(["avacodo","beets","carrots","tomato","banana"])
+fruits & vegetables #this is a set intersection
+
+set(["tamato"])
+fruits  - vegetables #this is a set difference
+set(["avacodo","banana"])
+vegetables - fruits 
