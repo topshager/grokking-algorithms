8bb9dc5111b98c5fb24b77015c8d9ab0bfa3ce9c
topshager <rem28777@gmail.com>
2025-03-28 18:37:51 +0200

    Update map_function.py

--- map_function.py
+++ map_function.py
@@ -1,5 +1,8 @@
 #arr1 = [1,2,3,4,5]
 #arr2 = map(lambda x:2 *x ,arr1)
 
-arr1 = # A list of Urls
-arr2 = map(download_page, arr1)
+#arr1 = # A list of Urls
+#arr2 = map(download_page, arr1)
+
+arr1 = [ 1,2,3,4,5]
+reduce(lambda x,y:x+y,arr1)
